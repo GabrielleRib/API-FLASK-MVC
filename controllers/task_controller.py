1a9d2d4cdb43bb281e9e7bf41cf57dd84a892bf9
Gabrielle Ribeiro <gabriellerpadua@gmail.com>
2025-09-08 10:53:55 -0300

    Atividade API Flask MVC

--- controllers/task_controller.py
+++ controllers/task_controller.py
@@ -7,7 +7,9 @@ class TaskController:
 
     @staticmethod
     def list_tasks():
+        tasks = Task.query.all()
         # TODO buscar todas as tarefas do banco de dados
+
         tasks = None 
         return render_template("tasks.html", tasks=tasks)
 
@@ -15,21 +17,34 @@ class TaskController:
     def create_task():
         
         if request.method == "POST":
-            
+            title = request.form["title"]
+            description = request.form["description"]
+            user_id = request.form["user_id"]
             # TODO capturar dados do formulário (title, description, user_id)
+
+            new_task = Task(title=title, description=description, user_id=user_id)
             # TODO criar um novo objeto Task com os dados capturados
+
+            db.session.add(new_task)
+            db.session.commit()
             # TODO adicionar no db.session e dar commit
             pass
 
             return redirect(url_for("list_tasks"))
-
+        
+        #?users = User.query.all()
         # TODO buscar todos os usuários para exibir no <select> do formulário
         users = None
         return render_template("create_task.html", users=users)
     
     @staticmethod
     def update_task_status(task_id):
+        task_to_update = Task.query.get(id)
         # TODO buscar a tarefa pelo id
+
+        if task_to_update:
+            task_to_update.status = "Concluído" 
+            db.session.commit
         # TODO: se existir, alternar status entre "Pendente" e "Concluído" e dar commit na alteração
         pass 
 
@@ -37,8 +52,12 @@ class TaskController:
 
     @staticmethod
     def delete_task(task_id):
-        
+        task_to_delete = Task.query.get(id)
         # TODO buscar a tarefa pelo id
+
+        if task_to_delete:
+            db.session.delete(task_to_delete)
+            db.session.commit()
         # TODO: se ela existir, remover do db.session e dar commit
         pass 
     
--- models/task.py
+++ models/task.py
@@ -2,13 +2,18 @@ from models import db
 
 class Task(db.Model):
     __tablename__ = "tasks"
-
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(100), nullable=False)
+    #user_id = 
+    status = db.Column(db.String(100), default="Pendente")
+    #user = User
     # TODO: Define os campos e o relacionamento da tabela Task
-    # - id: chave primária da tarefa
-    # - title: título da tarefa (obrigatório)
-    # - description: descrição detalhada da tarefa (obrigatório)
+    # - id: chave primária da tarefa (OK)
+    # - title: título da tarefa (obrigatório) (OK)
+    # - description: descrição detalhada da tarefa (obrigatório)(OK)
     # - user_id: chave estrangeira que conecta a tarefa a um usuário (não nulo)
-    # - status: indica o estado da tarefa, padrão "Pendente"
+    # - status: indica o estado da tarefa, padrão "Pendente"(OK)
     # - user: relacionamento com a classe User, usando back_populates="tasks" para criar o vínculo bidirecional
 
     def __repr__(self):
